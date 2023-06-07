e65215f86aa7995321c863dee49dfb4e34836aa5
VInicius Schuelter <48526428+viniciusschuelter@users.noreply.github.com>
2023-06-07 15:47:48 -0300

    feat(big-bang): big bang intersection observer

--- projects/ng-primitives/src/lib/ng-primitives.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'lib-ng-primitives',
-  template: ` <p>ng-primitives works!</p> `,
-  styles: [],
-})
-export class NgPrimitivesComponent {}
