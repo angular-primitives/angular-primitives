caed28b99ca1bfb3eb10176d3ce77eb7b902652c
VInicius Schuelter <48526428+viniciusschuelter@users.noreply.github.com>
2023-06-07 13:37:25 -0300

    feat(big-bang): big bang lint approach

--- projects/ng-primitives/src/lib/ng-primitives.component.ts
+++ projects/ng-primitives/src/lib/ng-primitives.component.ts
@@ -2,14 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'lib-ng-primitives',
-  template: `
-    <p>
-      ng-primitives works!
-    </p>
-  `,
-  styles: [
-  ]
+  template: ` <p>ng-primitives works!</p> `,
+  styles: [],
 })
-export class NgPrimitivesComponent {
-
-}
+export class NgPrimitivesComponent {}
